3ff495d620f48c44e84e923a96911a77426fcb34
daomcgill <77309217+daomcgill@users.noreply.github.com>
2023-10-05 13:47:46 -1000

    Update server.c

--- server.c
+++ server.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(void)
+//accept port number as argument
+int main(int argc, char *argv[])
 {
-   printf("Hello, World!\n");
-   return 0;
+   //arg to int
+   int port = atoi(argv[1]);
+
+   //write server output to file
+   FILE *file = freopen("output_server.txt", "w", stdout);
+
+   //create socket
+   int soc = socket(AF_INET, SOCK_STREAM, 0);
+   if (soc < 0)
+   {
+      printf("ERR: socket could not be created\n");
+      return 1;
+   }
+   //bind socket to port
+   
+
+
+   //close file
+   fclose(file);
+
 }
